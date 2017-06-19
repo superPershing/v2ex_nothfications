c012c9a551a41e72f531462b7ad28590105e4c8a
superPershing <727342027@qq.com>
2017-06-19 19:26:17 +0800

    fix email bug

--- reply_notification.js
+++ reply_notification.js
@@ -1,5 +1,7 @@
 require('phantomjs-prebuilt')
+// require('chromedriver')
 const webdriver = require('selenium-webdriver')
+const winston = require('winston')
 const By = webdriver.By
 const until = webdriver.until
 const later = require('later')
@@ -15,16 +17,14 @@ var sendEmail = process.argv[5]
 var sendEmailPassword = process.argv[6]
 var receiveEmail = process.argv[7]
 
-var textNotification = later.parse.text('every 20 min')
-var timerNotification = later.setInterval(getNotification, textNotification)
+var textNotification = later.parse.text('every 2 min')
 
 var checkInSchedule = {
-    schedules:
-    [
-        {h: [10], m: [32]}
-    ]
-  }
-var timerCheckIn = later.setInterval(checkIn, checkInSchedule)
+  schedules:
+  [
+        {h: [17], m: [0]}
+  ]
+}
 
 driver.get('https://www.v2ex.com/signin')
 driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input')).sendKeys(username)
@@ -32,13 +32,18 @@ driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/form/table/tbody/tr[2
 driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/form/table/tbody/tr[3]/td[2]/input[2]')).click()
 driver.get('https://www.v2ex.com/')
 
+var timerNotification = later.setInterval(getNotification, textNotification)
+var timerCheckIn = later.setInterval(checkIn, checkInSchedule)
+
 function getNotification () {
   driver.navigate().refresh()
   driver.findElement(By.xpath('//*[@id="Rightbar"]/div[2]/div[4]/a')).getText().then(function (text) {
     var unreadNum = Number(text.split(' ')[0])
-    console.log(unreadNum)
+    // console.log(unreadNum)
     if (unreadNum > 0) {
-      sendMail(sendEmailSmtp, sendEmail, sendEmailPassword, receiveEmail)
+      sendMail(sendEmailSmtp, sendEmail, sendEmailPassword, receiveEmail, 'notification!')
+    } else {
+      
     }
   })
 }
@@ -48,13 +53,14 @@ function checkIn () {
   driver.findElement(By.xpath('//*[@id="Rightbar"]/div[4]/div/a')).click()
   driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/input')).click()
   driver.get('https://www.v2ex.com/')
-  sendMail(sendEmailSmtp, sendEmail, sendEmailPassword, receiveEmail)
+  // console.log('Check in')
+  sendMail(sendEmailSmtp, sendEmail, sendEmailPassword, receiveEmail, 'checkIn!')
 }
 
 // driver.wait(until.titleIs('webdriver - Google Search'), 1000)
 // driver.quit()
 
-function sendMail (email, password, receiveEmail) {
+function sendMail (sendEmailSmtp, email, password, receiveEmail, emailText) {
   let transporter = nodemailer.createTransport({
     host: sendEmailSmtp,
     port: 465,
@@ -69,9 +75,9 @@ function sendMail (email, password, receiveEmail) {
   let mailOptions = {
     from: '<' + email + '>', // sender address
     to: receiveEmail, // list of receivers
-    subject: 'V2EX notification!', // Subject line
-    text: 'V2EX notification', // plain text body
-    html: '<b>V2EX notification</b>' // html body
+    subject: 'V2EX!', // Subject line
+    text: emailText, // plain text body
+    html: '<b>' + emailText + '</b>' // html body
   }
 
 // send mail with defined transport object
