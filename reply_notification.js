57bad4d465c9793e48dd1d1d387f4fa06c5841c1
superPershing <727342027@qq.com>
2017-06-25 16:15:02 +0800

    fix login failure bugs

--- reply_notification.js
+++ reply_notification.js
@@ -1,13 +1,13 @@
 require('phantomjs-prebuilt')
 // require('chromedriver')
 const webdriver = require('selenium-webdriver')
-const winston = require('winston')
+// const winston = require('winston')
 const By = webdriver.By
-const until = webdriver.until
+// const until = webdriver.until
 const later = require('later')
 const nodemailer = require('nodemailer')
 const driver = new webdriver.Builder()
-    .forBrowser('phantomjs')
+    .forBrowser('chrome')
     .build()
 
 var username = process.argv[2]
@@ -17,48 +17,63 @@ var sendEmail = process.argv[5]
 var sendEmailPassword = process.argv[6]
 var receiveEmail = process.argv[7]
 
-var textNotification = later.parse.text('every 20 min')
+// var textNotification = later.parse.text('every 10 min')
+// 有可能致使签到和检查通知同时操作，导致登录时重复发送用户名或密码，造成登录失败。
+var notificationSchedule = {
+  schedules:
+  [
+    {m: [0, 10, 20, 30, 40, 50]}
+  ]
+}
 
 var checkInSchedule = {
   schedules:
   [
-        {h: [17], m: [0]}
+        {h: [6], m: [25]}
   ]
 }
 
 login()
-
 function login () {
   driver.get('https://www.v2ex.com/signin')
-  driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input')).sendKeys(username)
-  driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/form/table/tbody/tr[2]/td[2]/input')).sendKeys(password)
+  driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input')).clear().then(() => {
+    console.log(username)
+    driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input')).sendKeys(username)
+  })
+  driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/form/table/tbody/tr[2]/td[2]/input')).clear().then(() => {
+    console.log(password)
+    driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/form/table/tbody/tr[2]/td[2]/input')).sendKeys(password)
+  })
   driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/form/table/tbody/tr[3]/td[2]/input[2]')).click()
-  driver.get('https://www.v2ex.com/')
 }
 
-var timerNotification = later.setInterval(getNotification, textNotification)
+var timerNotification = later.setInterval(getNotification, notificationSchedule)
 var timerCheckIn = later.setInterval(checkIn, checkInSchedule)
 
 function getNotification () {
+  login()
   driver.navigate().refresh()
-  driver.findElement(By.xpath('//*[@id="Rightbar"]/div[2]/div[4]/a')).getText().then(function (text) {
+  driver.findElement(By.xpath('//*[@id="Rightbar"]/div[2]/div[4]/strong/a')).getText().then(function (text) {
     var unreadNum = Number(text.split(' ')[0])
-    // console.log(unreadNum)
+    console.log(unreadNum)
     if (unreadNum > 0) {
       sendMail(sendEmailSmtp, sendEmail, sendEmailPassword, receiveEmail, 'notification!')
+      driver.findElement(By.xpath('//*[@id="Rightbar"]/div[2]/div[4]/strong/a')).click()
     } else {
 
     }
-  }).catch(() => login())
+    driver.get('https://www.v2ex.com/')
+  }).catch(() => driver.get('https://www.v2ex.com/'))
 }
 
 function checkIn () {
   login()
-  driver.findElement(By.xpath('//*[@id="Rightbar"]/div[4]/div/a')).click().catch(() => login())
-  driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/input')).click().catch(() => login())
+  driver.findElement(By.xpath('//*[@id="Rightbar"]/div[4]/div/a')).click()
+  driver.findElement(By.xpath('//*[@id="Main"]/div[2]/div[2]/input')).click().then(() => {
+    sendMail(sendEmailSmtp, sendEmail, sendEmailPassword, receiveEmail, 'checkIn!')
+  })
+  console.log('check in')
   driver.get('https://www.v2ex.com/')
-  // console.log('Check in')
-  sendMail(sendEmailSmtp, sendEmail, sendEmailPassword, receiveEmail, 'checkIn!')
 }
 
 // driver.wait(until.titleIs('webdriver - Google Search'), 1000)
